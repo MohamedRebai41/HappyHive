d30fd2e5059726f12850a30f6fcd1205285ad4e6
amina ghannem <amynaghannem@gmail.com>
2023-06-10 22:47:07 +0100

    created gift component

--- frontend/src/components/Gift.tsx
+++ frontend/src/components/Gift.tsx
@@ -1,14 +1,23 @@
+import {BsGift} from 'react-icons/bs'
+
 function Gift(){
     return(
-        <div className="bg-red flex h-full text-center max-w-screen-md w-full m-auto p-5 mb-[50%]">
-            <form className=''>
-                <div className="m-2">
-                    <label className="text-red">Choose Employee</label>
+        <div className="text-center max-w-screen-md w-[90%] m-auto p-5 rounded-md bg-gray-100 bg-opacity-90" style={{ width: 'fit-content' }}>
+            <form>
+                <div className="m-4">
+                    <label className="text-">Choose Employee</label>
+                </div>
+                <div className="m-4">
                     <select>
                     </select>
                 </div>
                 <div className="flex m-2">
-                    <input className="w-full h-12 px-2.5 rounded-md border-solid border-2 lg:w-[400px]"type="text" placeholder="Enter a message"/>
+                    <input className="w-full h-12 px-2.5 rounded-md border-solid border-2" type="text" placeholder="Enter a message"/>
+                </div>
+                <div className="m-4">
+                    <button className="h-12 text-white bg-secondary rounded-full w-12" type='submit'>
+                        <BsGift className="inline-block mb-1 text-primary"/>
+                    </button>
                 </div>
             </form>
         </div>
