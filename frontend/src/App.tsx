aa0ae3f15628e54f8d9c74a46e8ded570164f219
zk2k2 <ziedkharrat4juin@gmail.com>
2023-06-10 18:38:04 +0100

    first frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-200 h-200 bg-secondary"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      primary: "#222222",
+      secondary: "#fabf09",
+      tertiary: "#fabf09",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Inter", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
