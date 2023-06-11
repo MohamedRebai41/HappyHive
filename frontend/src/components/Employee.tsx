257f19e444ee2dda9de9b69484636586dd788cf6
amina ghannem <amynaghannem@gmail.com>
2023-06-11 01:51:32 +0100

    added employee type and data + employee component

---
+++ frontend/src/components/Employee.tsx
@@ -0,0 +1,31 @@
+import EmployeeType  from "../types/EmployeeType"
+
+// import { apiBaseUrl } from '../config';
+// import shoudDisplayDefaultImage from '../utils/shouldDisplayDefaultImage';
+
+const Employee: React.FC<{ index: number; employee: Partial<EmployeeType> }> = ({
+  index,
+  employee
+}) => {
+  return (
+    <div className='w-5/6 max-w-3xl'>
+      <div className='mb-3  flex w-full flex-row  gap-5 '>
+        <p className='font-black self-center'> {index} </p>
+        <img
+          src={
+            // shoudDisplayDefaultImage(employee.photo)
+            //   ? avatarGirl2
+            //   : apiBaseUrl + '/public/image/' + employee.photo
+            '/public/image/' + employee.photo
+          }
+          className='h-14 w-14 rounded-full border-2 border-black bg-white'
+        ></img>
+        <span className='self-center font-black'>{employee.fullName}</span>
+        <span className='self-center flex-grow text-right'>{employee.score}</span>
+      </div>
+      <hr className='border-t w-full border-black mb-4' />
+    </div>
+  );
+};
+
+export default Employee;
---
+++ frontend/src/data/EmployeeData.ts
@@ -0,0 +1,50 @@
+import Employee from "../types/EmployeeType";
+export const employees: Employee[] = [
+  {
+    photo: 'employee.jpg',
+    fullName: 'John Doe',
+    id: '001',
+    score: 80,
+    currency: 'USD',
+    email: 'johndoe@example.com',
+    post: 'Software Developer',
+  },
+  {
+    photo: 'employee2.jpg',
+    fullName: 'Jane Smith',
+    id: '002',
+    score: 95,
+    currency: 'EUR',
+    email: 'janesmith@example.com',
+    post: 'Product Manager',
+  },
+  {
+    photo: 'employee.jpg',
+    fullName: 'Michael Johnson',
+    id: '003',
+    score: 70,
+    currency: 'GBP',
+    email: 'michaeljohnson@example.com',
+    post: 'Marketing Specialist',
+  },
+  {
+    photo: 'employee.jpg',
+    fullName: 'Emily Davis',
+    id: '004',
+    score: 85,
+    currency: 'USD',
+    email: 'emilydavis@example.com',
+    post: 'Graphic Designer',
+  },
+  {
+    photo: 'employee.jpg',
+    fullName: 'David Wilson',
+    id: '005',
+    score: 90,
+    currency: 'CAD',
+    email: 'davidwilson@example.com',
+    post: 'Data Analyst',
+  },
+];
+
+export default employees;
